061564670839ec40efee81c5f7b2ac2ce1d5b61d
Julian Ehiem <julianehiem@Julians-iMac.local>
2020-06-24 16:18:09 -0400

    appended more colors and created color name view area with some css styling

--- js/script.js
+++ js/script.js
@@ -1,16 +1,3 @@
-//Choose a random color
-// const button = document.querySelector('button')
-// const body = document.querySelector('body')
-// const colors = ['red', 'green', 'blue', 'yellow', 'pink', 'purple']
-
-// body.style.backgroundColor = 'violet'
-// button.addEventListener('click', changeBackground)
-
-// function changeBackground(){
-// const colorIndex= parseInt(Math.random()*colors.length)
-// body.style.backgroundColor = colors[colorIndex]
-// }
-
 // Connecting to the DOM
 const body = document.querySelector("body");
 const button = document.querySelector("button");
@@ -27,11 +14,13 @@ function loadColor() {
     if (this.status == 200) {
       let color = JSON.parse(this.responseText);
       const colorIndex = parseInt(Math.random() * color.length);
-
       bgColor.innerHTML = color[colorIndex].name;
       body.style.backgroundColor = color[colorIndex].hex;
-      //console.log(color[0].name);
     }
+  };
+
+  xhr.onerror = function () {
+    alert("Something went wrong!");
   };
 
   xhr.send();
