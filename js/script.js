1a9e83c964f88554acfaa9c4afc0af872096c09d
Julian Ehiem <julianehiem@Julians-iMac.local>
2020-06-24 14:28:16 -0400

    Added colors.json and finalized project

--- js/script.js
+++ js/script.js
@@ -10,3 +10,29 @@
 // const colorIndex= parseInt(Math.random()*colors.length)
 // body.style.backgroundColor = colors[colorIndex]
 // }
+
+// Connecting to the DOM
+const body = document.querySelector("body");
+const button = document.querySelector("button");
+const bgColor = document.querySelector("#backgroundColor");
+
+// Event Listener added
+button.addEventListener("click", loadColor);
+
+function loadColor() {
+  let xhr = new XMLHttpRequest();
+  xhr.open("GET", "colors.json", true);
+
+  xhr.onload = function () {
+    if (this.status == 200) {
+      let color = JSON.parse(this.responseText);
+      const colorIndex = parseInt(Math.random() * color.length);
+
+      bgColor.innerHTML = color[colorIndex].name;
+      body.style.backgroundColor = color[colorIndex].hex;
+      //console.log(color[0].name);
+    }
+  };
+
+  xhr.send();
+}
